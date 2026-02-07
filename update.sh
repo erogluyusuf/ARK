a2c2c0219ccd250b4e5d0ba9bf56e07cbd17fb43
Yusuf Eroğlu <erogluyusuf@example.com>
2026-02-07 10:08:48 +0000

    update: 2026-02-07 10:08 otomatik güncelleme

--- update.sh
+++ update.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo "[!] GitHub'dan güncel veriler kontrol ediliyor..."
+git pull origin main --rebase
+
 echo "[!] Değişiklikler taranıyor..."
 git add .
+
 echo "[!] Paket mühürleniyor..."
 git commit -m "update: $(date +'%Y-%m-%d %H:%M') otomatik güncelleme"
+
 echo "[!] GitHub'a uçuruluyor..."
 git push origin main
-echo "[OK] Tüm değişiklikler artık GitHub'da canlı!"
+
+echo "[OK] İşlem tamam, her şey GitHub'da!"
